81c8b5bbd3c68f41c0f28ad3ee176a0123965eaa
shau vargas <Vargasshau@gmail.com>
2025-11-22 17:53:42 -0600

    config

--- Mod_config/config/db.config.js
+++ Mod_config/config/db.config.js
@@ -1,8 +1,8 @@
 export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
-export const VISITA_URL = process.env.VISITA || "http://localhost:4000";
-export const CLIENTE_URL = process.env.CLIENTE || "http://localhost:3000";
-export const EMPLEADO_URL = process.env.EMPLEADO || "http://localhost:5000";
-export const CONFIG_URL = process.env.CONFIG || "http://localhost:2000";
+export const VISITA_URL = process.env.VISITA_URL || "http://localhost:4000";
+export const CLIENTE_URL = process.env.CLIENTE_URL || "http://localhost:3000";
+export const EMPLEADO_URL = process.env.EMPLEADO_URL || "http://localhost:5000";
+export const CONFIG_URL = process.env.CONFIG_URL || "http://localhost:2000";
 
 export const DB_HOST = process.env.DB_HOST || "localhost"
 export const DB_PORT = process.env.DB_PORT || 5432
